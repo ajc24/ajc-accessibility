4c5b7818aecfe11ef19130d81df2a71f015dd0ed
ajc24 <anthonyjohnc@gmail.com>
2020-07-11 19:18:44 +0100

    update build 11/07/2020

--- build/test-development/AccessibilityDev.js
+++ build/test-development/AccessibilityDev.js
@@ -7,16 +7,7 @@ exports.default = void 0;
 
 var _jestAxe = require("jest-axe");
 
-var _pa11y = _interopRequireDefault(require("pa11y"));
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
-
-/* Individual test folder names */
-const accessibilityDirName = 'accessibility';
-const reportsDirName = 'reports';
-const testsDirName = 'tests';
 /* Test data */
-
 const maximumTestTimeout = 60000;
 
 class AccessibilityDev {
@@ -38,40 +29,6 @@ class AccessibilityDev {
   static async runJestAxe(componentHtml) {
     const results = await (0, _jestAxe.axe)(componentHtml);
     return results;
-  }
-  /**
-   * Executes the HTML at the specified URL / filename against pa11y
-   * and checks that HTML template for accessibility related issues
-   * @param {string} componentUrlOrFilename
-   * @param {string} testTitle
-   * @returns {boolean}
-   */
-
-
-  static async runPa11y(componentUrlOrFilename, testTitle) {
-    /* Execute the accessibility test on the HTML file and process the results */
-    const results = await (0, _pa11y.default)(componentUrlOrFilename);
-    let testPassed = true;
-
-    if (results.issues.length > 0) {
-      let errorMessage = '\n--------------------------------------------------\n';
-      errorMessage += ` Title of failing test  : ${results.documentTitle}\n`;
-      errorMessage += ` URL / path to test file: ${results.pageUrl}\n`;
-      errorMessage += ` Issues Found           : ${results.issues.length}\n`;
-      results.issues.forEach(function (issue) {
-        errorMessage += `\n Accessibility Code: ${issue.code}\n`;
-        errorMessage += ` Type              : ${issue.type}\n`;
-        errorMessage += ` Type code         : ${issue.typeCode}\n`;
-        errorMessage += ` Failure message   : ${issue.message}\n`;
-        errorMessage += ` Element affected  : ${issue.context}\n`;
-        errorMessage += ` CSS selector      : ${issue.selector}\n`;
-      });
-      errorMessage += '--------------------------------------------------\n';
-      console.log(errorMessage);
-      testPassed = false;
-    }
-
-    return testPassed;
   }
 
 }
